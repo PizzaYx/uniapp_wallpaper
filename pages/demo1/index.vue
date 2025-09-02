fa2225e57d0ab73585782117a2306e22167173de
PizzaYx <371812114@qq.com>
2025-09-02 17:43:36 +0800

    1
    1

---
+++ pages/demo1/index.vue
@@ -0,0 +1,50 @@
+<template>
+	<view>
+		<swiper indicator-dots="true" indicator-color="rgba(0, 0, 0, .3)" indicator-active-color="#007aff" autoplay="true" circular="true" interval="2000" vertical>
+			<swiper-item>
+				1111
+			</swiper-item>
+			<swiper-item>
+				2222
+			</swiper-item>
+			<swiper-item>
+				3333
+			</swiper-item>
+			<swiper-item>
+				4444
+			</swiper-item>
+		</swiper>
+	</view>
+	
+	<view>
+		<image src="/static/logo.png" mode="" class="pic1" ></image>
+		
+		
+	</view>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+.pic1{
+	width: 100px;
+	height: 100px;
+}	
+swiper{
+	width: 100vw;
+	height: 200px;
+	// box-sizing: border-box;
+	border: 1px solid green;
+	swiper-item{
+		width: 100%;
+		height: 100%;
+		background: pink;
+	}
+	swiper-item:nth-child(2n){
+		background: orange;
+	}
+}
+
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,41 @@
+<template>
+	<view hover-class="boxHover">
+		<view class="inner" hover-class="innerHover" hover-stop-propagation>内部元素</view>
+	</view>
+
+<scroll-view scroll-x="true" class="scrllView">
+	<view class="box">scroll子元素</view>
+	<view class="box">scroll子元素</view>
+	<view class="box">scroll子元素</view>
+	<view class="box">scroll子元素</view>
+	<view class="box">scroll子元素</view>
+	<view class="box">scroll子元素</view>
+	<view class="box">scroll子元素</view>
+	<view class="box">scroll子元素</view>
+	<view class="box">scroll子元素</view>
+	<view class="box">scroll子元素</view>
+	<view class="box">scroll子元素</view>
+	<view class="box">scroll子元素</view>
+	
+</scroll-view>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+.scrllView{
+	width: 80%;
+	height: 220px;
+	border: 1px solid red;
+	white-space: nowrap;
+}
+.box{
+	width: 100px;
+	height: 100px;
+	background: goldenrod;
+	display: inline-block;
+	margin: 5px;
+}	
+</style>
