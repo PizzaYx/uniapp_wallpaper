d6e2091607af683cdb047003b7c573251e13018b
PizzaYx <45346905+PizzaYx@users.noreply.github.com>
2025-09-05 22:09:49 +0800

    1
    1

---
+++ uniappV3Demo2/pages/demo5/demo5.vue
@@ -0,0 +1,19 @@
+<template>
+	<view class="layout">
+		<view class="box">
+			
+		</view>
+	</view>
+</template>
+
+<script setup>
+	
+</script>
+
+<style lang="scss" scoped>
+.box{
+	width: 200rpx;
+	height: 200rpx;
+	background: orange;
+}
+</style>
